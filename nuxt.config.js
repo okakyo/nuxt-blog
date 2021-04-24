fdf6542418a18b3c2e8d3180c501ae0938eccaca
Kyohei Oka <0622okakyo@gmail.com>
2021-04-24 15:19:00 +0900

    Vercel へデプロイするための環境構築

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,6 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
-    '@nuxtjs/feed',
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
